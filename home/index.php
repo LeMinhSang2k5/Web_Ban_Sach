7453b6c07ba683445b6d61e782f3304ff93df6f1
Minh Sang <thailand0987654321.llms@gmail.com>
2025-06-08 22:15:40 +0700

    Update connect backend databse

--- home/index.php
+++ home/index.php
@@ -1,4 +1,8 @@
 <?php
+// Bật hiển thị lỗi để debug
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 session_start();
 
 $error = [
@@ -18,10 +22,26 @@ function isActiveForm($formName, $activeForm)
 {
     return $formName === $activeForm ? 'active' : '';
 }
+
+// Xử lý trang hiển thị
+$page = $_GET['page'] ?? 'home';
+
+include 'db.php';
 ?>
 
 <?php include 'includes/header.php'; ?>
 <main>
-    <?php include 'pages/home.php'; ?>
+    <?php 
+    switch($page) {
+        case 'children':
+            include 'pages/chilldrenBook.php';
+            break;
+        default:
+            include 'pages/home.php';
+            break;
+    }
+    ?>
 </main>
 <?php include 'includes/footer.php'; ?>
+
+
