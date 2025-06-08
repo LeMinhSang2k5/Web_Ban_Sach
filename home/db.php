7279188539b1664a099465c762f87d5c5adb506e
Minh Sang <thailand0987654321.llms@gmail.com>
2025-06-08 22:15:09 +0700

    Update connect database

--- home/db.php
+++ home/db.php
@@ -1,11 +1,23 @@
 <?php
+// Bật hiển thị lỗi để debug
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Thông tin kết nối database
 $host = "localhost";
-$user = "root";
-$password = "";
-$database = "users_db";
+$username = "root";  // Thay đổi thành root vì đây là user mặc định của XAMPP
+$password = "";      // Mặc định XAMPP không có password
+$database = "mydb";
 
-$conn = new mysqli($host, $user, $password, $database);
+// Tạo kết nối
+$conn = mysqli_connect($host, $username, $password, $database);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Kiểm tra kết nối
+if (!$conn) {
+    die("Kết nối thất bại: " . mysqli_connect_error());
 }
+
+// Set charset
+mysqli_set_charset($conn, "utf8");
+
+?>
