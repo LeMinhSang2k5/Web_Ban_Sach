d54ec8bab9b0afde5b03f115bdeff7e3e329ec69
linh0526 <linhsubin007@gmail.com>
2025-07-05 21:06:21 +0700

    up

--- home/includes/header.php
+++ home/includes/header.php
@@ -94,18 +94,18 @@ require_once __DIR__ . '/../config.php';
     </header>
     <div class="main-contain">
         <div class="wrapper">
-            <span class="icon-close"><i class="fa-solid fa-xmark"></i></span>
+            <span class="icon-close"><svg style="width:28px;height: 28px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="#ffffff" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg></span>
             <div class="form-box <?= isActiveForm('login', $activeForm); ?> login ">
                 <h2>Đăng Nhập</h2>
                 <?= showError($error['login']); ?>
                 <form action="config.php" method="POST">
                     <div class="input-box">
-                        <span class="icon"><i class="fa-solid fa-envelope"></i></span>
+                        <span class="icon"><svg style="width:24px;height: 24px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z"/></svg></span>
                         <input type="text" name="username" required>
                         <label>Tên đăng nhập</label>
                     </div>
                     <div class="input-box">
-                        <span class="icon"><i class="fa-solid fa-lock"></i></span>
+                        <span class="icon"><svg style="width:24px;height: 24px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="#000000" d="M144 144l0 48 160 0 0-48c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192l0-48C80 64.5 144.5 0 224 0s144 64.5 144 144l0 48 16 0c35.3 0 64 28.7 64 64l0 192c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 256c0-35.3 28.7-64 64-64l16 0z"/></svg></span>
                         <input type="password" name="password" required>
                         <label>Mật khẩu</label>
                     </div>
@@ -125,17 +125,17 @@ require_once __DIR__ . '/../config.php';
                 <?= showError($error['register']); ?>
                 <form action="config.php" method="POST">
                     <div class="input-box">
-                        <span class="icon"><i class="fa-solid fa-user"></i></span>
+                        <span class="icon"><svg style="width:24px;height: 24px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z"/></svg></span>
                         <input type="text" name="username" required>
                         <label>Tên người dùng</label>
                     </div>
                     <div class="input-box">
-                        <span class="icon"><i class="fa-solid fa-envelope"></i></span>
+                        <span class="icon"><svg style="width:24px;height: 24px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg></span>
                         <input type="email" name="email" required>
                         <label>Email</label>
                     </div>
                     <div class="input-box">
-                        <span class="icon"><i class="fa-solid fa-lock"></i></span>
+                        <span class="icon"><svg style="width:24px;height: 24px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="#000000" d="M144 144l0 48 160 0 0-48c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192l0-48C80 64.5 144.5 0 224 0s144 64.5 144 144l0 48 16 0c35.3 0 64 28.7 64 64l0 192c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 256c0-35.3 28.7-64 64-64l16 0z"/></svg></span>
                         <input type="password" name="password" required>
                         <label>Mật khẩu</label>
                     </div>
